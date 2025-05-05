c8b3c4b284d0e5e578dc957147cb6807d0f74241
AsherSeawall <aseawall@numurus.com>
2025-05-04 20:58:58 -0700

    image viewer

--- scripts/image_viewer_app_node.py
+++ scripts/image_viewer_app_node.py
@@ -42,7 +42,6 @@ from nepi_sdk import nepi_utils
 from nepi_api.node_if import NodeClassIF
 from nepi_api.messages_if import MsgIF
 from nepi_api.system_if import SaveDataIF
-from nepi_api.system_if import SaveCfgIF
 
 
 #########################################
