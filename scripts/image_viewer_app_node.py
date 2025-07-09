33a247bd4f3fbe6b8a8d3559ea331e6f278c4b3d
Jason Seawall <jseawall@numurus.com>
2025-07-09 16:37:54 -0700

    **

--- scripts/image_viewer_app_node.py
+++ scripts/image_viewer_app_node.py
@@ -236,14 +236,21 @@ class NepiImageViewerApp(object):
     self.publish_status()
 
   def resetCb(self,do_updates = True):
-    if do_updates:
+      self.msg_if.pub_warn("Reseting")
+      if self.node_if is not None:
         pass
-    self.initCb(do_updates = do_updates)
+      if do_updates == True:
+        pass
+      self.initCb(do_updates = do_updates)
+
 
   def factoryResetCb(self,do_updates = True):
-    if do_updates:
+      self.msg_if.pub_warn("Factory Reseting")
+      if self.node_if is not None:
         pass
-    self.initCb(do_updates = do_updates)
+      if do_updates == True:
+        pass
+      self.initCb(do_updates = do_updates)
 
 
   ###################
